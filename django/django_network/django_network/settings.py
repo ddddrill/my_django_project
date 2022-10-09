1a0bdbc12b5ee3e81a26f7aea1c7406168791053
ddddrill <110777399+ddddrill@users.noreply.github.com>
2022-10-09 21:41:41 +0300

    09.10.2022

--- django/django_network/django_network/settings.py
+++ django/django_network/django_network/settings.py
@@ -1,3 +1,4 @@
+# -*- coding utf-8 -*-
 """
 Django settings for django_network project.
 
@@ -10,6 +11,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,6 +118,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#https://docs.djangoproject.com/en/4.1/ref/settings/#static-root
+
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
+#https://docs.djangoproject.com/en/4.1/ref/settings/#staticfiles-dirs
+
+STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR,'static')
+]
+
+#https://docs.djangoproject.com/en/4.1/ref/settings/#staticfiles-finders
+
+STATIC_FINDER = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media') # наша папка для всякого рода медиа
+MEDIA_URL= '/media/' # как она будет отображаться
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
